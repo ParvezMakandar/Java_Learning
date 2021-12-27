9120ce3cb7737544e2a1f8fb95ac8b84b2a510a5
parvezhm050 <parvezhmakandar5@gmail.com>
2021-12-27 20:38:40 +0530

    Quotient_and_Remainder

---
+++ src/day_5/Quotient_and_Remainder.java
@@ -0,0 +1,16 @@
+package day_5;
+
+public class Quotient_and_Remainder {
+
+    public static void main(String[] args)
+    {
+
+        int dividend = 554, divisor = 9;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
+    }
+}
