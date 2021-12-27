7ef29d0606c086cc6b1ff68b01ecff1f306801c9
parvezhm050 <parvezhmakandar5@gmail.com>
2021-12-27 16:54:31 +0530

    Leap_Year

---
+++ src/day_5/Leap_Year.java
@@ -0,0 +1,30 @@
+package day_5;
+import java.util.Scanner;
+
+public class Leap_Year
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter any year:");
+        int year = s.nextInt();
+        boolean flag;
+        if(year % 400 == 0)
+        {
+            flag = true;
+        }
+        else if (year % 100 == 0)
+        {
+            flag = false;
+        }
+        else flag = year % 4 == 0;
+        if(flag)
+        {
+            System.out.println("Year "+year+" is a Leap Year");
+        }
+        else
+        {
+            System.out.println("Year "+year+" is not a Leap Year");
+        }
+    }
+}
