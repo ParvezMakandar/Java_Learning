881ae923b63bbaa976b702fa79f69e9f1da6f00b
parvezhm050 <parvezhmakandar5@gmail.com>
2022-01-09 23:25:54 +0530

    Linked List

---
+++ src/day_14/LinkedList/Linked_List.java
@@ -0,0 +1,106 @@
+package day_14.LinkedList;
+
+public class Linked_List
+{
+    public static void main(String[] arg)
+    {
+        Linked_List List = new Linked_List();
+        List.addAtStart(70);
+        List.addAtStart(56);
+        List.insertAfter(List.head, 30);
+        List.search(30);
+        List.display();
+    }
+    static class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
+    public void display()
+    {
+        Node current = head;
+
+        if (head == null)
+        {
+            System.out.println("List is empty");
+            return;
+        }
+        System.out.println("Sequence of linked list: ");
+        while (current != null)
+        {
+            System.out.print(current.data + "-> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public void addAtStart(int newdata)
+    {
+        Node newNode = new Node(newdata);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            Node temp = head;
+            head = newNode;
+            head.next = temp;
+        }
+    }
+
+    public void insertAfter(Node preNode, int newdata)
+    {
+        if (preNode == null)
+        {
+            System.out.println("invalid");
+            return;
+        }
+        Node newNode = new Node(newdata);
+        newNode.next = preNode.next;
+        preNode.next = newNode;
+    }
+
+    public void search(Integer data)
+    {
+        boolean flag = false;
+        int i = 1;
+        Node current = head;
+        if (head == null)
+        {
+            System.out.println("empty");
+        }
+        while (current != null)
+        {
+            if (current.data == data)
+            {
+                System.out.println("found");
+                flag = true;
+                break;
+            }
+            i++;
+            current = current.next;
+        }
+        if (flag)
+        {
+            System.out.println("element is present at:" + i);
+        }
+        else
+        {
+            System.out.println("element is absent");
+        }
+    }
+
+}
